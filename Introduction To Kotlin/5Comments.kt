d056692bbc43204eb4e7ab802e0716e974ff5e1c
allenJames7777 <alluvido_200000000403@uic.edu.ph>
2022-08-24 00:45:47 +0800

    Introduction To Kotlin

---
+++ Introduction To Kotlin/5Comments.kt
@@ -0,0 +1,5 @@
+fun main() {
+    //Code prints "Howdy, partner!" to the terminal
+    println("Howdy, partner!")
+    /*Learned how to print , comment , use main function in kotlin */ 
+  }
---
+++ Introduction To Kotlin/8Review and Resources.kt
@@ -0,0 +1,7 @@
+// Add your code below
+fun main(){
+    println(2022-21)
+    println("Do your best and God will do the rest!")
+   // main function done ,  println done 
+  }
+  

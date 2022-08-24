86de77f430ba190bfeb514476909ba4388764d93
allenJames777 <alluvido_200000000403@uic.edu.ph>
2022-08-24 23:10:33 +0800

    commit

---
+++ (b) Data Types & Variables/UsingTextVariables/5CharacterEscapeSequence.kt
@@ -0,0 +1,13 @@
+/* \n Inserts a new line
+\t Inserts a tab
+\r Inserts a carriage return
+\' Inserts a single quote
+\" Inserts a double quote
+\\ Inserts a backslash
+\$ Inserts the dollar symbol*/
+fun main() {
+
+    // Add the correct escape sequences to the text below to achieve the output shown in the instructions 
+  
+    print("Dear Learner 👋, \"I hope you're enjoying the Learn Kotlin \r course. You've made tremendous progress\'. Keep going! \t And remember, Knowledge is \n power. (Frances Beacon). With love, Codey")
+  }
